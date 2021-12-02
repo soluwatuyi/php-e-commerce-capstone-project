e9563062dc2b6cc214e2b72a279f764d167247b3
Oluwatuyi Segun <63879692+soluwatuyi@users.noreply.github.com>
2021-12-02 10:46:40 +0100

    Update config.php

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','l9tslhne_shopping');
-define('DB_PASS' ,'-RWU8FNZv1v+');
-define('DB_NAME', 'l9tslhne_shopping');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'shopx');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
